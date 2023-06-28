e35e31f783a3b483e32dfdd8899e87db245b8605
Alruses <87943246+Alruses@users.noreply.github.com>
2023-06-27 17:52:51 -0700

    Add files via upload

--- level.py
+++ level.py
@@ -56,12 +56,35 @@ class RandomLevel(Level):
         #floorNum = int(round(self.w * self.h//4 + random.randint(5, 20)))
 
         count = 0
-
+        currTile = [1,1]
         while count < floorNum:
-            #homework: plan out the wandering generation algorithim
-                #pick a random direciton after each check
-                #also check if tile is a wall or floor
-                #bonus: check if its an outer wall and skip those
+            #this ensures our starting tile stays a floor
+            if game_map[1][1] != "f":
+                game_map[1][1] = "f"
+                count += 1
+            #this piece of code turns whatever the currently selected tile into the floor if it isn't one already
+            if game_map[currTile[0]][currTile[1]] != "f":
+                game_map[currTile[0]][currTile[1]] = "f"
+                count += 1
 
+            #pick a random direction to move in, then adjust the values of currTile to match
+            move = random.randint(1,4)
+            if move == 1 and currTile[0] > 2:
+                currTile[0] -= 1
+            elif move == 2 and currTile[0] < (self.h - 3):
+                currTile[0] += 1
+            elif move == 3 and currTile[0] < (self.w - 3):
+                currTile[1] += 1
+            elif move == 4 and currTile[0] > 2:
+                currTile[1] -= 1
+
+        for i in range(len(game_map)):
+            for j in range(len(game_map[i])):
+                if game_map[i][j] == "wa":
+                    #add to sprite group - Instantiate a tile here
+                    self.walls.add(Tile(images["wa"], (i*32, j*32), "wall"))
+                elif game_map[i][j] == "f":
+                    #add to other sprite group - Instantiate a tile here
+                    self.floors.add(Tile(images["f"], (i*32, j*32), "floor"))
 
 #class blankLevel(Level)
